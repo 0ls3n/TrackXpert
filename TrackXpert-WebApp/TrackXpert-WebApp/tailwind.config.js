9d6306f7aaa2b3cd8ee1e919904ea7c42ce5d7bb
Rasmus Olsen <rasmus782@gmail.com>
2025-01-20 01:38:54 +0100

    Replaced bootstrap with TailwindCss and made a new NavBar

---
+++ TrackXpert-WebApp/TrackXpert-WebApp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/**/*.{razor,html}",
+        "./Components/**/*.{razor,html}",
+        "./wwwroot/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
